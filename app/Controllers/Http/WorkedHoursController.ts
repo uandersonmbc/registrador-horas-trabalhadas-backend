c78036706f4f210a4f6c89e06e3c1129872be207
Uanderson Nunes <uandersonmbc@hotmail.com>
2020-12-04 01:31:00 -0300

    Subindo melhorias de dados

--- app/Controllers/Http/WorkedHoursController.ts
+++ app/Controllers/Http/WorkedHoursController.ts
@@ -4,18 +4,25 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 import WorkedHour from 'App/Models/WorkedHour'
 
+import { dayWeek, week, weekTotal } from 'App/Querys/WorkedHour'
+
 export default class WorkedHoursController {
   public async index({ request, response, auth }: HttpContextContract) {
     const user = await auth.authenticate()
-    const data = request.only(['start', 'end'])
-    const {
-      rows,
-    } = await Database.rawQuery(
-      `SELECT to_char(wh."start"::timestamp, 'IW') as week_number, SUM(wh."end"::timestamp - wh."start"::timestamp) AS total_number_hours FROM worked_hours wh WHERE user_id = ? AND "start" BETWEEN ? AND ? AND type_id = ? GROUP BY week_number`,
-      [user.id, data.start, data.end, 1]
-    )
-
-    return response.json(rows)
+    const data = request.only(['start', 'end', 'weekTotal'])
+    try {
+      let res: any
+      if (data.start && data.end === undefined) {
+        res = await Database.rawQuery(dayWeek, [user.id, data.start])
+      } else if (data.start && data.end && !data.weekTotal) {
+        res = await Database.rawQuery(week, [user.id, data.start, data.end])
+      } else {
+        res = await Database.rawQuery(weekTotal, [user.id, data.start, data.end])
+      }
+      return response.json(res.rows)
+    } catch (error) {
+      return response.status(error.status).json({ msg: 'Deu ruim', error })
+    }
   }
 
   public async store({ request, response, auth }: HttpContextContract) {
@@ -36,7 +43,7 @@ export default class WorkedHoursController {
       const workedHour = await WorkedHour.create(data)
       return response.json(workedHour)
     } catch (error) {
-      return response.json({ msg: 'Deu ruim' })
+      return response.status(error.status).json({ msg: 'Deu ruim' })
     }
   }
 
@@ -53,7 +60,7 @@ export default class WorkedHoursController {
         message: workedHours[0] ? 'Dados Atualizados' : 'Erro ao atualizar os dados ',
       })
     } catch (error) {
-      return response.json({ message: 'Deu ruim' })
+      return response.status(error.status).json({ message: 'Deu ruim' })
     }
   }
 
@@ -69,7 +76,7 @@ export default class WorkedHoursController {
         message: workedHours[0] ? 'Registro apagado' : 'Erro ao apagar registro',
       })
     } catch (error) {
-      return response.json({ message: 'Deu ruim' })
+      return response.status(error.status).json({ message: 'Deu ruim' })
     }
   }
 }
---
+++ app/Querys/WorkedHour.ts
@@ -0,0 +1,24 @@
+export const dayWeek: string = `
+select wh.id, t2."name" as activity, p2."name" as project,project_id, type_id, "start", "end", (wh."end"::timestamp - wh."start"::timestamp) AS hours from worked_hours wh
+join "types" t2 on wh.type_id = t2.id
+join projects p2 on wh.project_id = p2.id
+where wh.user_id = ? and wh."start"::date = ?
+`
+
+export const week: string = `
+select
+	SUM(case when wh.type_id = 1 then  wh."end"::timestamp - wh."start"::timestamp else '00:00:00' end) AS total,
+	SUM(case when wh.type_id = 2 then  wh."end"::timestamp - wh."start"::timestamp else '00:00:00' end) AS coffee,
+	SUM(case when wh.type_id = 3 then  wh."end"::timestamp - wh."start"::timestamp else '00:00:00' end) AS lunch,
+	SUM(case when wh.type_id = 4 then  wh."end"::timestamp - wh."start"::timestamp else '00:00:00' end) AS "absent",
+	SUM(case when wh.type_id = ANY ('{2,3,4}'::int[]) then  wh."end"::timestamp - wh."start"::timestamp else '00:00:00' END) AS pause,
+	min(wh."start") as start_date_timestamp,
+	max(wh."end") as end_date_timestamp
+from worked_hours wh
+where wh.user_id = ? and "start" between ? and ?
+group by wh."start"::date
+`
+
+export const weekTotal: string = `
+select to_char(wh."start"::timestamp, 'IW') as "week", SUM(wh."end"::timestamp - wh."start"::timestamp) AS total from worked_hours wh where wh.user_id = ? and "start" between ? and ? and project_id = 1 and type_id = 1 group by "week";
+`
